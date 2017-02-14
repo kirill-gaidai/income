afc348ad013130b872b373401dc644ef0657e7da
Kirill Gaidai <kirill.gaidai@gmail.com>
2017-02-14 08:41:56 +0300

    Added constructor to service exception

--- src/main/java/org/kirillgaidai/income/exception/IncomeServiceException.java
+++ src/main/java/org/kirillgaidai/income/exception/IncomeServiceException.java
@@ -1,4 +1,9 @@
 package org.kirillgaidai.income.exception;
 
 public class IncomeServiceException extends RuntimeException {
+
+    public IncomeServiceException(final String message, final Object... args) {
+        super (String.format(message, args));
+    }
+
 }
