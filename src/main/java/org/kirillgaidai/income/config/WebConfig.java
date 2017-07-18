6889b53a2a9e65ab2a4118a30def88a94069c3f9
kirill-gaidai <kirill.gaidai@gmail.com>
2017-07-18 17:38:48 +0300

    Modified web layer config and added js resources

--- src/main/java/org/kirillgaidai/income/config/WebConfig.java
+++ src/main/java/org/kirillgaidai/income/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -13,17 +14,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(value = {"org.kirillgaidai.income.web", "org.kirillgaidai.income.exception"})
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public InternalResourceViewResolver internalResourceViewResolver() {
-        final InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/WEB-INF/pages/");
-        internalResourceViewResolver.setSuffix(".jsp");
-        return internalResourceViewResolver;
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(new InternalResourceViewResolver("/WEB-INF/pages/", ".jsp"));
     }
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
     }
 
 }
