b8f95e4beaebda3643badf4ca32819e9efca7883
Kirill Gaidai <kirill.gaidai@gmail.com>
2017-07-25 22:20:03 +0300

    Removed bug with LocalDate as request param

--- src/main/java/org/kirillgaidai/income/dao/impl/BalanceDao.java
+++ src/main/java/org/kirillgaidai/income/dao/impl/BalanceDao.java
@@ -49,7 +49,7 @@ public class BalanceDao implements IBalanceDao {
                 "ON (b2.account_id = b1.account_id) AND (b2.day = b1.day) ORDER BY b2.account_id";
         Map<String, Object> params = new HashMap<>();
         params.put("account_ids", accountIds);
-        params.put("last_day", lastDay);
+        params.put("last_day", Date.valueOf(lastDay));
         return namedParameterJdbcTemplate.query(sql, params, balanceEntityRowMapper);
     }
 
