34bbeb8d7a8c973c8e96e6b9ed78baf08eac5284
kirill-gaidai <kirill.gaidai@gmail.com>
2017-08-04 16:21:17 +0300

    Added comments to generic entity and dao interfaces

--- src/main/java/org/kirillgaidai/income/dao/entity/IGenericEntity.java
+++ src/main/java/org/kirillgaidai/income/dao/entity/IGenericEntity.java
@@ -1,5 +1,8 @@
 package org.kirillgaidai.income.dao.entity;
 
+/**
+ * Generic entity interface. Generic entity has id
+ */
 public interface IGenericEntity {
 
     Integer getId();
--- src/main/java/org/kirillgaidai/income/dao/intf/IGenericDao.java
+++ src/main/java/org/kirillgaidai/income/dao/intf/IGenericDao.java
@@ -5,18 +5,58 @@ import org.kirillgaidai.income.dao.entity.IGenericEntity;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Defines basic operations for DAO with entities implementing {@link IGenericEntity}
+ *
+ * @param <T> - generic entity class
+ */
 public interface IGenericDao<T extends IGenericEntity> {
 
+    /**
+     * Returns all entities from DB table
+     *
+     * @return entity list
+     */
     List<T> getEntityList();
 
+    /**
+     * Returns entities with specified ids from DB
+     *
+     * @param ids - entity ids set
+     * @return entity list
+     */
     List<T> getEntityList(Set<Integer> ids);
 
+    /**
+     * Returns entity with specified id from DB
+     *
+     * @param id - entity id
+     * @return entity
+     */
     T getEntity(Integer id);
 
+    /**
+     * Creates new entity in DB and sets its id
+     *
+     * @param entity - new entity
+     * @return number of inserted rows
+     */
     int insertEntity(T entity);
 
+    /**
+     * Updates entity with specified in DB
+     *
+     * @param entity - entity
+     * @return number of updated rows
+     */
     int updateEntity(T entity);
 
+    /**
+     * Deletes entity from DB by its id
+     *
+     * @param id - entity id
+     * @return number of deleted rows
+     */
     int deleteEntity(Integer id);
 
 }
