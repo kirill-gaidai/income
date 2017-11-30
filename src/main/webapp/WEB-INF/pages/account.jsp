a1db0f328fd2064a500889e7bdd69e6a5c0615e7
Kirill Gaidai <kirill.gaidai@gmail.com>
2017-11-30 23:20:11 +0300

    Added account UI

---
+++ src/main/webapp/WEB-INF/pages/account.jsp
@@ -0,0 +1,63 @@
+<%@page contentType="text/html;charset=utf-8" pageEncoding="utf-8" %>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+<spring:url var="stylesUrl" value="/css/styles.css"/>
+<spring:url var="jQueryUrl" value="/js/jquery-3.2.1.js"/>
+<spring:url var="scriptUrl" value="/js/account.js"/>
+<spring:url var="appCtx" value="/"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accounts</title>
+    <link rel="stylesheet" type="text/css" href="${stylesUrl}"/>
+    <script src="${jQueryUrl}"></script>
+    <script>
+        var appCtx = "${appCtx}".substr(0, "${appCtx}".length - 1);
+    </script>
+</head>
+<body>
+
+<h1>Accounts</h1>
+
+<form name="form" id="form">
+    <table class="form">
+        <tr>
+            <td><label for="id">ID</label></td>
+            <td><input type="text" name="id" id="id" readonly></td>
+        </tr>
+        <tr>
+            <td><label for="currencyId">Currency</label></td>
+            <td><select name="currencyId" id="currencyId"></select></td>
+        </tr>
+        <tr>
+            <td><label for="sort">Sort</label></td>
+            <td><input type="text" name="sort" id="sort"></td>
+        </tr>
+        <tr>
+            <td><label for="title">Title</label></td>
+            <td><input type="text" name="title" id="title"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" name="new" id="new" value="New">
+                <input type="submit" name="save" id="save" value="Save">
+            </td>
+        </tr>
+    </table>
+</form>
+
+<table class="list" id="list">
+    <tr>
+        <th>ID</th>
+        <th>Currency code</th>
+        <th>Currency title</th>
+        <th>Sort</th>
+        <th>Title</th>
+        <th>Actions</th>
+    </tr>
+</table>
+
+<script src="${scriptUrl}"></script>
+</body>
---
+++ src/main/webapp/js/account.js
@@ -0,0 +1,173 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var listElem = document.getElementById("list").firstElementChild;
+
+    var formElem = document.getElementById("form");
+    var idElem = document.getElementById("id");
+    var currencyIdElem = document.getElementById("currencyId");
+    var sortElem = document.getElementById("sort");
+    var titleElem = document.getElementById("title");
+    var saveBtnElem = document.getElementById("save");
+    var newBtnElem = document.getElementById("new");
+
+    var entityList = [];
+    var currentEntity = null;
+
+    var resourceUrl = appCtx + "/rest/account";
+    var currencyResourceUrl = appCtx + "/rest/currency";
+
+    jQuery.getJSON(currencyResourceUrl, function (data) {
+        data.forEach(function (entity) {
+            var optionElem = document.createElement("option");
+            optionElem.value = entity.id;
+            optionElem.text = "[" + entity.code + "] " + entity.title;
+            currencyIdElem.add(optionElem);
+        });
+        currencyIdElem.value = null;
+    });
+
+    jQuery.getJSON(resourceUrl, function (data) {
+        entityList = data;
+        entityList.forEach(function (entity) {
+            appendRow(entity);
+        });
+    });
+
+    formElem.addEventListener("submit", doOnFormSubmit);
+    saveBtnElem.addEventListener("click", doOnSaveBtnClick);
+    newBtnElem.addEventListener("click", doOnNewBtnClick);
+
+    function doOnFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    function doOnNewBtnClick() {
+        clearForm();
+    }
+
+    function doOnSaveBtnClick() {
+        var linkedRow = currentEntity ? currentEntity.linkedRow : null;
+        var entity = getEntityFromForm();
+        clearForm();
+
+        jQuery.ajax({
+            url: resourceUrl,
+            method: linkedRow ? "put" : "post",
+            data: JSON.stringify(entity),
+            contentType: "application/json",
+            dataType: "json",
+            success: linkedRow ? doOnPutSuccess : doOnPostSuccess,
+            error: doOnError
+        });
+
+        function doOnPutSuccess(data) {
+            updateRow(linkedRow, data);
+            copyEntity(data, linkedRow.linkedEntity);
+        }
+
+        function doOnPostSuccess(data) {
+            appendRow(data);
+            entityList.push(data)
+        }
+
+        function doOnError() {
+            console.log("error");
+        }
+    }
+
+    function doOnEditBtnClick() {
+        currentEntity = this.parentElement.parentElement.linkedEntity;
+        fillForm(currentEntity);
+    }
+
+    function doOnDeleteBtnClick() {
+        var linkedRow = this.parentElement.parentElement;
+        var entity = linkedRow.linkedEntity;
+
+        jQuery.ajax({
+            url: resourceUrl + "/" + entity.id,
+            method: "delete",
+            success: doOnDeleteSuccess,
+            error: doOnError
+        });
+
+        function doOnDeleteSuccess() {
+            for (var index = 0; index < entityList.length; index++) {
+                if (entityList[index] === entity) {
+                    listElem.removeChild(entity.linkedRow);
+                    entityList.splice(index, 1);
+                }
+            }
+        }
+
+        function doOnError() {
+            console.log("error");
+        }
+    }
+
+    function getEntityFromForm() {
+        return {
+            id: idElem.value ? idElem.value : null,
+            currencyId: currencyIdElem.value ? +currencyIdElem.value : null,
+            sort: sortElem.value,
+            title: titleElem.value
+        };
+    }
+
+    function clearForm() {
+        currentEntity = null;
+        idElem.value = "";
+        currencyIdElem.value = null;
+        sortElem.value = "";
+        titleElem.value = "";
+    }
+
+    function fillForm(entity) {
+        idElem.value = entity.id;
+        currencyIdElem.value = entity.currencyId;
+        sortElem.value = entity.sort;
+        titleElem.value = entity.title;
+    }
+
+    function appendRow(entity) {
+        var rowElem = listElem.appendChild(document.createElement("tr"));
+        rowElem.linkedEntity = entity;
+        entity.linkedRow = rowElem;
+        var cellElem = rowElem.appendChild(document.createElement("td"));
+        cellElem.innerText = entity.id;
+        cellElem = rowElem.appendChild(document.createElement("td"));
+        cellElem.innerText = entity.currencyCode;
+        cellElem = rowElem.appendChild(document.createElement("td"));
+        cellElem.innerText = entity.currencyTitle;
+        cellElem = rowElem.appendChild(document.createElement("td"));
+        cellElem.innerText = entity.sort;
+        cellElem = rowElem.appendChild(document.createElement("td"));
+        cellElem.innerText = entity.title;
+        cellElem = rowElem.appendChild(document.createElement("td"));
+        var btnElem = cellElem.appendChild(document.createElement("button"));
+        btnElem.innerText = "Edit";
+        btnElem.onclick = doOnEditBtnClick;
+        btnElem = cellElem.appendChild(document.createElement("button"));
+        btnElem.innerText = "Delete";
+        btnElem.onclick = doOnDeleteBtnClick;
+    }
+
+    function updateRow(rowElem, entity) {
+        rowElem.children[0].innerText = entity.id;
+        rowElem.children[1].innerText = entity.currencyCode;
+        rowElem.children[2].innerText = entity.currencyTitle;
+        rowElem.children[3].innerText = entity.sort;
+        rowElem.children[4].innerText = entity.title;
+    }
+
+    function copyEntity(source, target) {
+        target.id = source.id;
+        target.currencyCode = source.currencyCode;
+        target.currencyTitle = source.currencyTitle;
+        target.sort = source.sort;
+        target.title = source.title;
+    }
+
+});
