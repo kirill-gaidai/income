27eb24add7057f82b931c6f6b54ba5c046dcfe7d
Kirill Gaidai <kirill.gaidai@effective-soft.com>
2018-03-06 18:01:33 +0300

    Replaced jndi datasource with database connection pool (for Heroku)

--- src/main/java/org/kirillgaidai/income/config/PersistenceConfig.java
+++ src/main/java/org/kirillgaidai/income/config/PersistenceConfig.java
@@ -1,12 +1,15 @@
 package org.kirillgaidai.income.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.kirillgaidai.income.exception.IncomeException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @ComponentScan("org.kirillgaidai.income.dao")
@@ -14,9 +17,32 @@ public class PersistenceConfig {
 
     @Bean
     public DataSource dataSource() {
-        JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        } catch (URISyntaxException e) {
+            throw new IncomeException(e.getMessage());
+        }
+
+        String driverClassName = "org.postgresql.Driver";
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+                + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driverClassName);
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+
+        /*JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
         jndiDataSourceLookup.setResourceRef(true);
-        return jndiDataSourceLookup.getDataSource("jdbc/income");
+        return jndiDataSourceLookup.getDataSource("jdbc/income");*/
     }
 
     @Bean
--- src/main/java/org/kirillgaidai/income/config/WebConfig.java
+++ src/main/java/org/kirillgaidai/income/config/WebConfig.java
@@ -32,9 +32,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         ).addResourceLocations("/resources/");
     }
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
-    }
+    }*/
 
 }
